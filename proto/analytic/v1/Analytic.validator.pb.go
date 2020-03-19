b17a44677a7f745ae267224d72b634b3978bfe45
Baeclatant <baeclatant@localhost.localdomain>
2020-03-19 08:25:41 +0800

    change metadata type from string to google.protobuf.Any

--- proto/analytic/v1/Analytic.validator.pb.go
+++ proto/analytic/v1/Analytic.validator.pb.go
@@ -8,6 +8,7 @@ import (
 	math "math"
 	proto "github.com/golang/protobuf/proto"
 	_ "github.com/golang/protobuf/ptypes/timestamp"
+	_ "github.com/golang/protobuf/ptypes/any"
 	github_com_mwitkow_go_proto_validators "github.com/mwitkow/go-proto-validators"
 )
 
@@ -20,6 +21,11 @@ func (this *EventLog) Validate() error {
 	if this.Date != nil {
 		if err := github_com_mwitkow_go_proto_validators.CallValidatorIfExists(this.Date); err != nil {
 			return github_com_mwitkow_go_proto_validators.FieldError("Date", err)
+		}
+	}
+	if this.Metadata != nil {
+		if err := github_com_mwitkow_go_proto_validators.CallValidatorIfExists(this.Metadata); err != nil {
+			return github_com_mwitkow_go_proto_validators.FieldError("Metadata", err)
 		}
 	}
 	return nil
