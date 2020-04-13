e29f3e7be3f3c9432724f83464ad1e85f09a366e
Pradipta Baskara <fenezemageraint@gmail.com>
2020-04-13 20:08:36 +0800

    NAL-61 Feat/add nalar event message (#48)
    * NAL-61 add NalarEvent message that is going to be publish to nats stream server
    
    * NAL-61 add NalarEventMessage
    
    Co-authored-by: Baeclatant <baeclatant@localhost.localdomain>

--- proto/analytic/v1/Analytic.validator.pb.go
+++ proto/analytic/v1/Analytic.validator.pb.go
@@ -6,9 +6,10 @@ package prixa_analytics_v1
 import (
 	fmt "fmt"
 	math "math"
+
 	proto "github.com/golang/protobuf/proto"
-	_ "github.com/golang/protobuf/ptypes/timestamp"
 	_ "github.com/golang/protobuf/ptypes/any"
+	_ "github.com/golang/protobuf/ptypes/timestamp"
 	github_com_mwitkow_go_proto_validators "github.com/mwitkow/go-proto-validators"
 )
 
@@ -26,6 +27,24 @@ func (this *EventLog) Validate() error {
 	if this.Metadata != nil {
 		if err := github_com_mwitkow_go_proto_validators.CallValidatorIfExists(this.Metadata); err != nil {
 			return github_com_mwitkow_go_proto_validators.FieldError("Metadata", err)
+		}
+	}
+	return nil
+}
+func (this *NalarEvent) Validate() error {
+	if this.EventLog != nil {
+		if err := github_com_mwitkow_go_proto_validators.CallValidatorIfExists(this.EventLog); err != nil {
+			return github_com_mwitkow_go_proto_validators.FieldError("EventLog", err)
+		}
+	}
+	if this.Reply != nil {
+		if err := github_com_mwitkow_go_proto_validators.CallValidatorIfExists(this.Reply); err != nil {
+			return github_com_mwitkow_go_proto_validators.FieldError("Reply", err)
+		}
+	}
+	if this.Result != nil {
+		if err := github_com_mwitkow_go_proto_validators.CallValidatorIfExists(this.Result); err != nil {
+			return github_com_mwitkow_go_proto_validators.FieldError("Result", err)
 		}
 	}
 	return nil
