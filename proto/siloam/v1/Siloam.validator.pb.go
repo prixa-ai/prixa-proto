97b5b05c46b38972eb08ae78dd7307c02a75c976
farhan <farhan@prixa.ai>
2020-04-15 14:00:45 +0700

    Add telemedicine cancel appointment API and Add scheduleId in appointment booking data

--- proto/siloam/v1/Siloam.validator.pb.go
+++ proto/siloam/v1/Siloam.validator.pb.go
@@ -7,10 +7,10 @@ import (
 	fmt "fmt"
 	math "math"
 	proto "github.com/golang/protobuf/proto"
-	_ "github.com/golang/protobuf/ptypes/timestamp"
-	_ "github.com/mwitkow/go-proto-validators"
 	_ "github.com/golang/protobuf/ptypes/empty"
 	_ "github.com/golang/protobuf/ptypes/wrappers"
+	_ "github.com/golang/protobuf/ptypes/timestamp"
+	_ "github.com/mwitkow/go-proto-validators"
 	regexp "regexp"
 	github_com_mwitkow_go_proto_validators "github.com/mwitkow/go-proto-validators"
 )
@@ -327,18 +327,15 @@ func (this *PostAppointmentResponseData) Validate() error {
 func (this *PostAppointmentData) Validate() error {
 	return nil
 }
-
-var _regex_CancelAppointmentRequestData_UserId = regexp.MustCompile(`^([a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[4][a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12})?$`)
-
 func (this *CancelAppointmentRequestData) Validate() error {
 	if this.AppointmentId == "" {
 		return github_com_mwitkow_go_proto_validators.FieldError("AppointmentId", fmt.Errorf(`value '%v' must not be an empty string`, this.AppointmentId))
 	}
-	if !_regex_CancelAppointmentRequestData_UserId.MatchString(this.UserId) {
-		return github_com_mwitkow_go_proto_validators.FieldError("UserId", fmt.Errorf(`value '%v' must be a string conforming to regex "^([a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[4][a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12})?$"`, this.UserId))
-	}
 	if this.UserId == "" {
 		return github_com_mwitkow_go_proto_validators.FieldError("UserId", fmt.Errorf(`value '%v' must not be an empty string`, this.UserId))
+	}
+	if this.Source == "" {
+		return github_com_mwitkow_go_proto_validators.FieldError("Source", fmt.Errorf(`value '%v' must not be an empty string`, this.Source))
 	}
 	return nil
 }
