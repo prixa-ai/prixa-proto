d5e5e84bdf8b6d6a1a87d2d63d4f6bb5d1dd83a3
Baeclatant <baeclatant@localhost.localdomain>
2020-04-16 18:37:16 +0800

    add body on SendCovidForm rpc

--- proto/diagnostic/v1/Diagnostic.pb.gw.go
+++ proto/diagnostic/v1/Diagnostic.pb.gw.go
@@ -326,18 +326,15 @@ func local_request_DiagnosticService_GetDiagnosticStatistics_0(ctx context.Conte
 
 }
 
-var (
-	filter_DiagnosticService_SendCovidForm_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
-)
-
 func request_DiagnosticService_SendCovidForm_0(ctx context.Context, marshaler runtime.Marshaler, client DiagnosticServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq SendCovidFormRequest
 	var metadata runtime.ServerMetadata
 
-	if err := req.ParseForm(); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
 	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_DiagnosticService_SendCovidForm_0); err != nil {
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
@@ -350,7 +347,11 @@ func local_request_DiagnosticService_SendCovidForm_0(ctx context.Context, marsha
 	var protoReq SendCovidFormRequest
 	var metadata runtime.ServerMetadata
 
-	if err := runtime.PopulateQueryParameters(&protoReq, req.URL.Query(), filter_DiagnosticService_SendCovidForm_0); err != nil {
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
