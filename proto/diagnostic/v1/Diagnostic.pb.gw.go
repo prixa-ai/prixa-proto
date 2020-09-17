894a86b28dcd90687480448ef9772c83dfd01a31
fenezema <fenezemageraint@gmail.com>
2020-09-17 13:26:24 +0700

    add additional response payload for diagnosticid checker

--- proto/diagnostic/v1/Diagnostic.pb.gw.go
+++ proto/diagnostic/v1/Diagnostic.pb.gw.go
@@ -538,6 +538,60 @@ func local_request_DiagnosticService_IsDiagnosisResultValid_0(ctx context.Contex
 
 }
 
+func request_DiagnosticService_IsDiagnosisResultValid_1(ctx context.Context, marshaler runtime.Marshaler, client DiagnosticServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq IsDiagnosisResultValidRequest
+	var metadata runtime.ServerMetadata
+
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["diagnosticSessionID"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "diagnosticSessionID")
+	}
+
+	protoReq.DiagnosticSessionID, err = runtime.String(val)
+
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "diagnosticSessionID", err)
+	}
+
+	msg, err := client.IsDiagnosisResultValid(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_DiagnosticService_IsDiagnosisResultValid_1(ctx context.Context, marshaler runtime.Marshaler, server DiagnosticServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq IsDiagnosisResultValidRequest
+	var metadata runtime.ServerMetadata
+
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["diagnosticSessionID"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "diagnosticSessionID")
+	}
+
+	protoReq.DiagnosticSessionID, err = runtime.String(val)
+
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "diagnosticSessionID", err)
+	}
+
+	msg, err := server.IsDiagnosisResultValid(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 // RegisterDiagnosticServiceHandlerServer registers the http handlers for service DiagnosticService to "mux".
 // UnaryRPC     :call DiagnosticServiceServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
@@ -840,6 +894,26 @@ func RegisterDiagnosticServiceHandlerServer(ctx context.Context, mux *runtime.Se
 		}
 
 		forward_DiagnosticService_IsDiagnosisResultValid_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	mux.Handle("GET", pattern_DiagnosticService_IsDiagnosisResultValid_1, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_DiagnosticService_IsDiagnosisResultValid_1(rctx, inboundMarshaler, server, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_DiagnosticService_IsDiagnosisResultValid_1(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -1184,6 +1258,26 @@ func RegisterDiagnosticServiceHandlerClient(ctx context.Context, mux *runtime.Se
 
 	})
 
+	mux.Handle("GET", pattern_DiagnosticService_IsDiagnosisResultValid_1, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_DiagnosticService_IsDiagnosisResultValid_1(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_DiagnosticService_IsDiagnosisResultValid_1(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -1217,6 +1311,8 @@ var (
 	pattern_DiagnosticService_GetAllContentCard_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"api", "v1", "contentcard"}, "", runtime.AssumeColonVerbOpt(true)))
 
 	pattern_DiagnosticService_IsDiagnosisResultValid_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3, 2, 4}, []string{"api", "v1", "diagnosis", "diagnosticSessionID", "validate"}, "", runtime.AssumeColonVerbOpt(true)))
+
+	pattern_DiagnosticService_IsDiagnosisResultValid_1 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"api", "v1", "prognosis", "diagnosticSessionID"}, "", runtime.AssumeColonVerbOpt(true)))
 )
 
 var (
@@ -1249,4 +1345,6 @@ var (
 	forward_DiagnosticService_GetAllContentCard_0 = runtime.ForwardResponseMessage
 
 	forward_DiagnosticService_IsDiagnosisResultValid_0 = runtime.ForwardResponseMessage
+
+	forward_DiagnosticService_IsDiagnosisResultValid_1 = runtime.ForwardResponseMessage
 )
